b710c2ad3cc93436892317ef7df7890ca3c566f9
Kurt Eckhardt <kurte@rockisland.com>
2020-01-08 08:05:31 -0800

    Fix GFX Font output of space character
    The code was bailing if w, h=0, so now we check that it is not a space.
    
    Maybe we should check the variables xo and yo...

--- ILI9341_t3n.cpp
+++ ILI9341_t3n.cpp
@@ -3683,7 +3683,8 @@ void ILI9341_t3n::drawGFXFontChar(unsigned int c) {
     GFXglyph *glyph  = gfxFont->glyph + (c - first);
     uint8_t   w     = glyph->width,
               h     = glyph->height;
-    if((w == 0) || (h == 0))  return;  // Is there an associated bitmap?
+    // wonder if we should look at xo, yo instead?         
+    if((w == 0 ||  h == 0)  && (c != 32))   return;  // Is there an associated bitmap?
 
     int16_t xo = glyph->xOffset; // sic
     int16_t yo = glyph->yOffset + gfxFont->yAdvance/2;
